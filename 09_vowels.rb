0e7bb91538acac702e20a0546ffe0a08c0f5f71f
danakalee <dana.ka.lee@gmail.com>
2019-04-08 14:16:59 +1000

    solved M stubbs Holidays

--- 09_vowels.rb
+++ 09_vowels.rb
@@ -12,4 +12,24 @@
 
 def vowels (string)
   # Your code here
+  phrase = string.chars
+  vowels = ["a", "e", "i", "o", "u"]
+  result = []
+  phrase.each do |element|
+    if vowels.include?(element) == true
+      result << element 
+    end 
+  end
+  return result
 end
+
+p vowels("hello world")
+p vowels("wow i'm doing it")
+
+#turn string into an array - name variable
+#create array with vowels
+#loop through string array and compare element with vowel array
+#if element matches array store in result array
+#continue for length of string array 
+#return result array
+
